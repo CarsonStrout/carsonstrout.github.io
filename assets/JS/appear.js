39997a2203efacd1e2fa0e232316cf224e66d038
Carson Strout <carson.strout42@gmail.com>
2023-12-14 15:56:41 -0700

    Added content loading

--- assets/JS/appear.js
+++ assets/JS/appear.js
@@ -1,33 +1,35 @@
-const fadeObjs = document.querySelectorAll(".fade1");
+document.addEventListener("DOMContentLoaded", () => {
+	const fadeObjs = document.querySelectorAll(".fade1");
 
-const observer = new IntersectionObserver((entries, observer) => {
-	entries.forEach((entry) => {
-		if (entry.isIntersecting) {
-			let obj = entry.target;
-			obj.classList.remove("loading");
-			obj.classList.add("loaded");
-			observer.unobserve(obj);
-		}
+	const observer = new IntersectionObserver((entries, observer) => {
+		entries.forEach((entry) => {
+			if (entry.isIntersecting) {
+				let obj = entry.target;
+				obj.classList.remove("loading");
+				obj.classList.add("loaded");
+				observer.unobserve(obj);
+			}
+		});
 	});
-});
 
-fadeObjs.forEach((obj) => {
-	observer.observe(obj);
-});
-
-const fadeObjs2 = document.querySelectorAll(".fade2");
-
-const observer2 = new IntersectionObserver((entries, observer2) => {
-	entries.forEach((entry) => {
-		if (entry.isIntersecting) {
-			let obj = entry.target;
-			obj.classList.remove("loading");
-			obj.classList.add("loaded-slow");
-			observer2.unobserve(obj);
-		}
+	fadeObjs.forEach((obj) => {
+		observer.observe(obj);
 	});
-});
 
-fadeObjs2.forEach((obj) => {
-	observer2.observe(obj);
+	const fadeObjs2 = document.querySelectorAll(".fade2");
+
+	const observer2 = new IntersectionObserver((entries, observer2) => {
+		entries.forEach((entry) => {
+			if (entry.isIntersecting) {
+				let obj = entry.target;
+				obj.classList.remove("loading");
+				obj.classList.add("loaded-slow");
+				observer2.unobserve(obj);
+			}
+		});
+	});
+
+	fadeObjs2.forEach((obj) => {
+		observer2.observe(obj);
+	});
 });
