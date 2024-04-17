e2851364b5964bf68f7f5f1af1a8f269301762f2
Carson Strout <carson.strout42@gmail.com>
2024-04-17 12:45:16 -0600

    Pauses videos when hidden

--- assets/JS/videoVisibility.js
+++ assets/JS/videoVisibility.js
@@ -3,8 +3,24 @@ document.addEventListener("DOMContentLoaded", function () {
 		button.addEventListener("click", function () {
 			var videoId = button.getAttribute("data-target");
 			var videoContainer = document.getElementById(videoId);
+			var iframe = videoContainer.querySelector("iframe");
+			if (!iframe) {
+				console.error("No iframe found in container with ID:", videoId);
+				return;
+			}
+
 			var display = window.getComputedStyle(videoContainer).display;
-			videoContainer.style.display = display === "none" ? "block" : "none";
+
+			if (display !== "none") {
+				videoContainer.style.display = "none";
+				var src = iframe.src;
+				iframe.src = "";
+				setTimeout(() => {
+					iframe.src = src;
+				}, 50);
+			} else {
+				videoContainer.style.display = "block";
+			}
 		});
 	});
 });
