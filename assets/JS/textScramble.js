06af075ecbc9f5083c115a09e01b85f34f5cad87
Carson Strout <carson.strout42@gmail.com>
2025-02-28 01:48:36 -0700

    Few small updates, turned off scrambling for now

--- assets/JS/textScramble.js
+++ assets/JS/textScramble.js
@@ -1,31 +1,31 @@
-const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-let intervals = [];
+// let intervals = [];
 
-const h2Elements = Array.from(document.querySelectorAll("h2"));
+// const h2Elements = Array.from(document.querySelectorAll("h2"));
 
-h2Elements.forEach((h2Element) => {
-	h2Element.addEventListener("mouseover", (event) => {
-		let iteration = 0;
-		clearInterval(intervals[h2Elements.indexOf(h2Element)]);
+// h2Elements.forEach((h2Element) => {
+// 	h2Element.addEventListener("mouseover", (event) => {
+// 		let iteration = 0;
+// 		clearInterval(intervals[h2Elements.indexOf(h2Element)]);
 
-		intervals[h2Elements.indexOf(h2Element)] = setInterval(() => {
-			event.target.innerText = event.target.innerText
-				.split("")
-				.map((letter, index) => {
-					if (index < iteration) {
-						return event.target.dataset.value[index];
-					}
+// 		intervals[h2Elements.indexOf(h2Element)] = setInterval(() => {
+// 			event.target.innerText = event.target.innerText
+// 				.split("")
+// 				.map((letter, index) => {
+// 					if (index < iteration) {
+// 						return event.target.dataset.value[index];
+// 					}
 
-					return letters[Math.floor(Math.random() * 26)];
-				})
-				.join("");
+// 					return letters[Math.floor(Math.random() * 26)];
+// 				})
+// 				.join("");
 
-			if (iteration >= event.target.dataset.value.length) {
-				clearInterval(intervals[h2Elements.indexOf(h2Element)]);
-			}
+// 			if (iteration >= event.target.dataset.value.length) {
+// 				clearInterval(intervals[h2Elements.indexOf(h2Element)]);
+// 			}
 
-			iteration += 1 / 3;
-		}, 30);
-	});
-});
+// 			iteration += 1 / 3;
+// 		}, 30);
+// 	});
+// });
